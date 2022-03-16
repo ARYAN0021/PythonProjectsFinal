de68d739f010e8e585b5e4b7d129672b4ac0c517
ARYAN0021 <77781900+ARYAN0021@users.noreply.github.com>
2022-03-16 22:41:59 +0530

    Add files via upload

--- Project97.py
+++ Project97.py
@@ -1,28 +1,41 @@
 # to import random package
 import random as rand
 
+level=input("Select your level : EASY HARD \n")
+
+if level == "EASY" :
+    number=rand.randint(1,10)
+    print("Number Guessing game")
+    print("Guess number between (1 to 9)")
+elif level == "HARD" :
+    number=rand.randint(1,100)
+    print("Number Guessing game")
+    print("Guess number between (1 to 100)")
+else:
+    number=rand.randint(1,10)
+    print("Number Guessing game")
+    print("Guess number between (1 to 9)")
+
 # variables
-number=rand.randint(1,10)
 chanceCount=0
 
-print("Guess number between (1 to 9)")
-guess=int(input("Enter your guess:"))
 
 #while loop
-while chanceCount < 4:
+while chanceCount < 5:
+    guess=int(input("Enter your guess:"))
 # decision making
     if guess < number :
         print("Entered number was too low : Enter number higher than",guess)
-        guess=int(input("Enter your guess:"))
         chanceCount+=1
     elif guess > number :
         print("Entered number was too high : Enter number lower than",guess)
-        guess=int(input("Enter your guess:"))
         chanceCount+=1
     elif guess == number :
         print("YOU WIN!!")
         break
 
-if not chanceCount < 4:
+if not chanceCount < 5:
     print("YOU LOSE")
+    print("CORRECT NUMBER IS",number)
+
 
